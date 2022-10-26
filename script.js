5b36636ee4affcc8d689c7efe118365e88f0b20b
sonya <86928699+allsuitablenamesarealreadytaken@users.noreply.github.com>
2022-10-26 20:52:28 +0200

    source code

---
+++ script.js
@@ -0,0 +1,240 @@
+const dictionary = ['iloveyou', 'absolute', 'admonish', 'advisory', 'agnostic', 'altruism', 'asteroid', 'aversion', 'bachelor', 'banished', 'banister', 'bankrupt', 'baritone', 'beautify', 'behavior', 'birdcage', 'blackout', 'blockage', 'bludgeon', 'botulism', 'boundary', 'brackish', 'bungalow', 'butchery', 'canister', 'category', 'cauldron', 'children', 'chipmunk', 'chlorine', 'clarinet', 'cloister', 'clothier', 'coliseum', 'complain', 'computer', 'courtesy', 'creation', 'culinary', 'cylinder', 'demolish', 'dialogue', 'dinosaur', 'discount', 'doctrine', 'domestic', 'dominate', 'dynamite', 'dyslexia', 'educator', 'emigrant', 'emulsify', 'enormity', 'favorite', 'ferocity', 'filament', 'flounder', 'flourish', 'fraction', 'fragment', 'frighten', 'fumigate', 'gasoline', 'goldfish', 'graceful', 'habitude', 'handgrip', 'handover', 'handsome', 'handwork', 'hangbird', 'hangover', 'hardline', 'harmonic', 'hayfield', 'hazelnut', 'headlock', 'headlong', 'headwork', 'hedonism', 'hedonist', 'heliport', 'herdsman', 'hindmost', 'holdfast', 'homeland', 'homesick', 'homespun', 'homeward', 'hornbeam', 'horsefly', 'horseman', 'hospital', 'hostelry', 'housefly', 'houseman', 'howitzer', 'huckster', 'humanely', 'humanist', 'humanity', 'humanize', 'humanoid', 'humblest', 'humbling', 'humoring', 'humorist', 'humpback', 'hungrily', 'hurdling', 'hurtling', 'hydrogen', 'hypnotic', 'hysteria', 'hysteric', 'icebound', 'ideogram', 'idolater', 'idolatry', 'impacted', 'imparted', 'implored', 'imported', 'imposter', 'improved', 'impudent', 'impugned', 'impurely', 'inchoate', 'inclosed', 'incubate', 'incurved', 'inductor', 'indulger', 'indurate', 'industry', 'inflamed', 'inflated', 'informal', 'informed', 'ingrowth', 'injector', 'insomuch', 'insulate', 'insulted', 'integral', 'intercom', 'interval', 'investor', 'involute', 'inwardly', 'islander', 'isolated', 'isometry', 'isopleth', 'isotherm', 'jalousie', 'jargoned', 'jaundice', 'jauntier', 'jauntily', 'jawboned', 'jealousy', 'jeopardy', 'jocundly', 'jointure', 'jokingly', 'jongleur', 'jostling', 'jousting', 'jowliest', 'jubilant', 'jubilate', 'judgment', 'jumbling', 'jumpiest', 'junglier', 'keyboard', 'keypunch', 'kilobyte', 'kilogram', 'klystron', 'knighted', 'knightly', 'knitwear', 'knowable', 'kohlrabi', 'laboring', 'lacewing', 'laconism', 'ladyship', 'lambency', 'lambskin', 'languish', 'lankiest', 'lardiest', 'latching', 'latchkey', 'laughter', 'launched', 'launcher', 'lavished', 'lavisher', 'layering', 'laywomen', 'leaching', 'leashing', 'leftward', 'legation', 'lethargy', 'licensor', 'lifeboat', 'ligament', 'ligature', 'liquored', 'literacy', 'loamiest', 'loathing', 'locating', 'locative', 'lockstep', 'lodestar', 'lodgment', 'longwise', 'lordship', 'lovebird', 'lovesick', 'lowering', 'luckiest', 'lukewarm', 'luminary', 'lumpfish', 'lumpiest', 'lunkhead', 'lurching', 'lymphoid', 'machined', 'madhouse', 'magister', 'magnetic', 'maidenly', 'majoring', 'majority', 'maledict', 'maligned', 'maligner', 'malinger', 'mandible', 'mangiest', 'mangrove', 'manicure', 'manifest', 'manifold', 'manliest', 'manpower', 'marbling', 'marching', 'marigold', 'marquise', 'masterly', 'matchbox', 'matronly', 'maturely', 'maturing', 'maverick', 'medaling', 'medalist', 'mediator', 'megavolt', 'meltdown', 'mensural', 'merciful', 'metaling', 'metaphor', 'methadon', 'methanol', 'metrical', 'milkwort', 'minstrel', 'minutely', 'mirthful', 'misbegot', 'misdealt', 'misheard', 'misjudge', 'misplace', 'misquote', 'misruled', 'mistaken', 'modality', 'modeling', 'modernly', 'modestly', 'modishly', 'modulate', 'moisture', 'molarity', 'moleskin', 'monarchy', 'monastic', 'monetary', 'monkfish', 'monsieur', 'monstera', 'moralist', 'morality', 'moralize', 'morbidly', 'mordancy', 'morphine', 'morticed', 'mortised', 'moseying', 'motherly', 'mouthier', 'mouthing', 'moveably', 'movingly', 'mucilage', 'muckiest', 'mulching', 'mulcting', 'murkiest', 'muscadel', 'muscatel', 'muscling', 'musicale', 'musingly', 'mustache', 'mutchkin', 'myriapod', 'mystique', 'mythical', 'narghile', 'natively', 'naturism', 'necropsy', 'neighbor', 'neoplasm', 'nepotism', 'neurotic', 'nightcap', 'nightjar', 'nimblest', 'nobelium', 'normalcy', 'notarize', 'novelist', 'nugatory', 'numeracy', 'obduracy', 'obdurate', 'obeisant', 'obituary', 'obligate', 'obscured', 'obstacle', 'obtained', 'obtusely', 'obviated', 'ochering', 'olibanum', 'oligarch', 'olympiad', 'opaquely', 'opaquing', 'operatic', 'ordinate', 'organdie', 'organism', 'organist', 'organize', 'orgasmic', 'orgastic', 'oriental', 'origanum', 'ornately', 'orphaned', 'osculate', 'outbrave', 'outbreak', 'outcried', 'outdrive', 'outfaced', 'outfield', 'outflank', 'outlawed', 'outlawry', 'outlined', 'outlived', 'outlying', 'outmarch', 'outpaced', 'outraced', 'outraged', 'outreach', 'outrival', 'outshine', 'outsider', 'outsized', 'outspeak', 'outspend', 'outvying', 'outweigh', 'overbusy', 'overcast', 'overhand', 'overhang', 'overhaul', 'overhung', 'overlaid', 'overlain', 'overland', 'overmuch', 'overpaid', 'overplay', 'overstay', 'ovulated', 'palimony', 'palmiest', 'palsying', 'panicked', 'panicled', 'parching', 'parhelic', 'parodist', 'paroling', 'paroquet', 'paroxysm', 'partible', 'particle', 'partying', 'pastiche', 'pastured', 'patchier', 'patchily', 'patching', 'pathogen', 'peaching', 'pearling', 'pectoral', 'peculiar', 'pedaling', 'pedantic', 'pedantry', 'pegboard', 'penlight', 'penumbra', 'perching', 'perianth', 'personal', 'perusing', 'pestling', 'petaloid', 'phenolic', 'phenylic', 'phonemic', 'phonetic', 'phoniest', 'phrasing', 'physical', 'physique', 'picayune', 'pictured', 'pilaster', 'pilchard', 'pilewort', 'pilotage', 'pinafore', 'pinochle', 'piquancy', 'pitchmen', 'plainest', 'planchet', 'plashing', 'plastery', 'platform', 'platinum', 'platonic', 'playsuit', 'playtime', 'pleading', 'pleasing', 'pleating', 'plectron', 'plectrum', 'pleonasm', 'pleurisy', 'ploughed', 'pluckier', 'plucking', 'plumaged', 'plumbery', 'plumbing', 'plushier', 'plutonic', 'poaching', 'podgiest', 'podiatry', 'poetical', 'polarity', 'polarize', 'polished', 'polisher', 'polymath', 'pomading', 'pomander', 'ponytail', 'porkiest', 'portable', 'postcard', 'postiche', 'postlude', 'postmark', 'postured', 'potsherd', 'pouching', 'poultice', 'poundage', 'powerful', 'powering', 'prankish', 'pratique', 'prickled', 'prideful', 'priestly', 'primeval', 'princely', 'probated', 'procaine', 'proclaim', 'prodigal', 'profaned', 'profiled', 'profited', 'promised', 'prostyle', 'proudest', 'provable', 'provably', 'province', 'proximal', 'psalmody', 'psyching', 'ptyalism', 'pubertal', 'publican', 'pudgiest', 'pulmonic', 'pulsated', 'punchier', 'punished', 'punitory', 'purblind', 'purchase', 'purslane', 'putridly', 'quackery', 'quacking', 'quagmire', 'quainter', 'quaintly', 'quakiest', 'qualmish', 'quantify', 'quashing', 'queasily', 'querying', 'questing', 'question', 'quickest', 'quipster', 'quotable', 'quotably', 'raftsmen', 'ragouted', 'rakishly', 'rambling', 'randiest', 'randomly', 'rangiest', 'ransomed', 'rationed', 'ravingly', 'ravished', 'rawboned', 'reaching', 'reacting', 'reaction', 'readjust', 'readying', 'rebating', 'rebuking', 'recoding', 'recusant', 'redshank', 'reducing', 'refacing', 'refusing', 'refuting', 'regional', 'relating', 'relation', 'relaunch', 'relaxing', 'relaying', 'remaking', 'removing', 'repaying', 'replying', 'reposing', 'republic', 'reputing', 'requital', 'rescuing', 'residual', 'resoling', 'resubmit', 'resuming', 'retaking', 'retching', 'revoking', 'rhapsody', 'rhyolite', 'rickshaw', 'rifleman', 'rightful', 'riposted', 'roaching', 'roasting', 'robustly', 'rockfish', 'rockiest', 'romanced', 'romantic', 'rondeaux', 'rotundly', 'roughest', 'roundest', 'roundish', 'rousting', 'rowdiest', 'rowdyism', 'royalism', 'royalist', 'rudiment', 'rumbling', 'ruminate', 'rumpling', 'rustical', 'rustling', 'sabering', 'saboteur', 'sacredly', 'saintdom', 'saliency', 'salmonid', 'saluting', 'sampling', 'sanctify', 'sandwich', 'sandwort', 'saponify', 'sardonic', 'sardonyx', 'sauteing', 'savoring', 'scalding', 'scalping', 'scamping', 'scandium', 'scantier', 'scantily', 'scarfing', 'scathing', 'scenario', 'scheming', 'schmaltz', 'scolding', 'scornful', 'scourged', 'scouring', 'scouting', 'scowling', 'scramble', 'scraping', 'scrawled', 'screwing', 'scrimped', 'scripted', 'scrofula', 'scrounge', 'scrupled', 'scrutiny', 'sculpted', 'sculptor', 'scurvily', 'scything', 'seafront', 'seamount', 'secantly', 'secondly', 'sectoral', 'securing', 'security', 'sedating', 'sedation', 'seducing', 'sedulity', 'seignory', 'semantic', 'seminary', 'semolina', 'seraglio', 'seraphic', 'seraphim', 'shackled', 'shadblow', 'shadower', 'shafting', 'shambled', 'shameful', 'shamrock', 'sharking', 'sharping', 'shearing', 'sheaving', 'sheikdom', 'shelduck', 'shelving', 'sherlock', 'shingled', 'shipload', 'shipmate', 'shipment', 'shipworm', 'shipyard', 'shocking', 'shortage', 'shorting', 'shoulder', 'shouting', 'showgirl', 'shrapnel', 'shrewdly', 'shrimped', 'shucking', 'shutdown', 'sidelong', 'sidewalk', 'signaled', 'signaler', 'silkworm', 'simulate', 'singable', 'singular', 'sinkhole', 'siphoned', 'skewbald', 'skinhead', 'skydiver', 'skylight', 'slacking', 'slangier', 'slighted', 'slighter', 'slipknot', 'slithery', 'slouched', 'sloughed', 'sludgier', 'slumbery', 'slumping', 'slurping', 'smacking', 'smarting', 'smearing', 'smelting', 'smidgeon', 'smirched', 'smocking', 'smoulder', 'smudgier', 'smudgily', 'snatched', 'snatcher', 'sneakily', 'snowbird', 'sobering', 'sobriety', 'sociable', 'sociably', 'societal', 'sodality', 'softback', 'software', 'solacing', 'solarium', 'soldiery', 'solitary', 'solitude', 'solvated', 'solvency', 'sombrely', 'somewhat', 'songbird', 'songlike', 'sourcing', 'southern', 'southpaw', 'souvenir', 'souvlaki', 'sowbread', 'spadeful', 'spangled', 'sparking', 'sparkled', 'spavined', 'speaking', 'spearing', 'specking', 'spectral', 'spectrum', 'specular', 'sphagnum', 'sphenoid', 'sphering', 'spheroid', 'spicular', 'spiracle', 'spiteful', 'splaying', 'splendor', 'splinted', 'splinter', 'splotchy', 'splurged', 'spoilage', 'spondaic', 'spongier', 'spongily', 'sporadic', 'sportily', 'sporting', 'sportive', 'spouting', 'sprained', 'sprawled', 'spraying', 'sprinkle', 'sprinted', 'sprocket', 'sprouted', 'sprucely', 'sprucing', 'spunkier', 'spurting', 'squadron', 'squander', 'squarely', 'squaring', 'squawked', 'squawker', 'squinted', 'squinter', 'squirmed', 'squirted', 'stabling', 'stacking', 'stalking', 'stamping', 'stanched', 'stanchly', 'stapling', 'starched', 'starlike', 'starling', 'starving', 'steadily', 'steading', 'stealing', 'steamily', 'steaming', 'sterling', 'stickler', 'stockade', 'stockier', 'stockily', 'stocking', 'stockman', 'stockmen', 'stodgier', 'stodgily', 'stomping', 'stonefly', 'storable', 'stormily', 'storming', 'strafing', 'strained', 'strangle', 'strawing', 'straying', 'strewing', 'stricken', 'strickle', 'stringed', 'strobila', 'strobile', 'stroking', 'strongly', 'strophic', 'studying', 'stumbled', 'stumbler', 'stumpier', 'stumping', 'stupider', 'stupidly', 'sturdily', 'sturgeon', 'stylized', 'subagent', 'sublimed', 'suborned', 'subpoena', 'subpolar', 'subtonic', 'subtopia', 'suchlike', 'suckling', 'suitable', 'suitably', 'sulfated', 'sulphate', 'sulphide', 'sunbaked', 'sunbathe', 'sunlight', 'superbly', 'superman', 'supernal', 'supertax', 'supinate', 'supinely', 'surfaced', 'surgical', 'suricate', 'surnamed', 'surplice', 'suzerain', 'swampier', 'swamping', 'swankier', 'swanlike', 'swarming', 'swathing', 'swearing', 'sweating', 'swerving', 'swindler', 'swingled', 'switched', 'sybarite', 'sycamore', 'syconium', 'symbolic', 'synaptic', 'syndetic', 'syndrome', 'synoptic', 'syphoned', 'syringed', 'tailored', 'thankful', 'thousand', 'tribunal', 'underway', 'velocity', 'vineyard', 'visceral', 'vocalist', 'weaponry', 'yourself'];
+
+const WORD_LENGTH = 8
+const FLIP_ANIMATION_DURATION = 500
+const DANCE_ANIMATION_DURATION = 500
+const keyboard = document.querySelector("[data-keyboard]")
+const alertContainer = document.querySelector("[data-alert-container]")
+const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2022, 0, 1)
+const msOffset = Date.now() - offsetFromDate
+// const dayOffset = msOffset / 1000 / 60 / 60 / 24
+// const targetWord = targetWords[Math.floor(dayOffset)]
+const targetWord = "iloveyou"
+
+startInteraction()
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick)
+  document.addEventListener("keydown", handleKeyPress)
+
+  let modal = document.getElementById("myModal");
+  let question = document.getElementById('question');
+  let span = document.getElementsByClassName("close")[0];
+  question.addEventListener('click', function(){
+    modal.style.display = "block";
+  });
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick)
+  document.removeEventListener("keydown", handleKeyPress)
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key)
+    return
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess()
+    return
+  }
+  let delete_img = document.getElementById('delete');
+  if (e.target.matches("[data-delete]") || delete_img.click()) {
+    deleteKey()
+    return
+  }
+
+}
+
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess()
+    return
+  }
+
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey()
+    return
+  }
+
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key)
+    return
+  }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles()
+  if (activeTiles.length >= WORD_LENGTH) return
+  const nextTile = guessGrid.querySelector(":not([data-letter])")
+  nextTile.dataset.letter = key.toLowerCase()
+  nextTile.textContent = key
+  nextTile.dataset.state = "active"
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles()
+  const lastTile = activeTiles[activeTiles.length - 1]
+  if (lastTile == null) return
+  lastTile.textContent = ""
+  delete lastTile.dataset.state
+  delete lastTile.dataset.letter
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()]
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Not enough letters")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter
+  }, "")
+
+  if (!dictionary.includes(guess)) {
+    showAlert("Not in word list")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  stopInteraction()
+  activeTiles.forEach((...params) => flipTile(...params, guess))
+}
+
+function flipTile(tile, index, array, guess) {
+  const letter = tile.dataset.letter
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`)
+  setTimeout(() => {
+    tile.classList.add("flip")
+  }, (index * FLIP_ANIMATION_DURATION) / 2)
+
+  tile.addEventListener(
+      "transitionend",
+      () => {
+        tile.classList.remove("flip")
+        if (targetWord[index] === letter) {
+          tile.dataset.state = "correct"
+          key.classList.add("correct")
+        } else if (targetWord.includes(letter)) {
+          tile.dataset.state = "wrong-location"
+          key.classList.add("wrong-location")
+        } else {
+          tile.dataset.state = "wrong"
+          key.classList.add("wrong")
+        }
+
+        if (index === array.length - 1) {
+          tile.addEventListener(
+              "transitionend",
+              () => {
+                startInteraction()
+                checkWinLose(guess, array)
+              },
+              { once: true }
+          )
+        }
+      },
+      { once: true }
+  )
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]')
+}
+
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div")
+  alert.textContent = message
+  alert.classList.add("alert")
+  alertContainer.prepend(alert)
+  if (duration == null) return
+
+  setTimeout(() => {
+    alert.classList.add("hide")
+    alert.addEventListener("transitionend", () => {
+      alert.remove()
+    })
+  }, duration)
+}
+
+function shakeTiles(tiles) {
+  tiles.forEach(tile => {
+    tile.classList.add("shake")
+    tile.addEventListener(
+        "animationend",
+        () => {
+          tile.classList.remove("shake")
+        },
+        { once: true }
+    )
+  })
+}
+
+function checkWinLose(guess, tiles) {
+  if (guess === targetWord) {
+    showAlert("You Win", 5000)
+    danceTiles(tiles)
+    displayModals()
+    stopInteraction()
+    return
+  }
+
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])")
+  if (remainingTiles.length === 0) {
+    showAlert(targetWord.toUpperCase(), null)
+    displayModals()
+    stopInteraction()
+  }
+
+}
+
+function danceTiles(tiles) {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance")
+      tile.addEventListener(
+          "animationend",
+          () => {
+            tile.classList.remove("dance")
+          },
+          { once: true }
+      )
+    }, (index * DANCE_ANIMATION_DURATION) / 5)
+  })
+}
+
+function displayModals() {
+  setTimeout(() => {
+      let result = document.getElementById("result");
+      let close = document.getElementsByClassName("result-close")[0];
+      document.getElementById("result_text").textContent = targetWord;
+      if (targetWord === "iloveyou") {
+        document.getElementById("anniversary").textContent = "Happy Anniversary, bby ♥";
+      }
+      result.style.display = "block";
+
+      close.onclick = function () {
+        result.style.display = "none";
+      }
+
+      window.onclick = function (event) {
+        if (event.target == result) {
+          result.style.display = "none";
+        }
+      }
+    }, 1500);
+}
